e9799b0cd51e7d865473ea22079d0db79aff45e1
jallubond <remeesarv155@gmail.com>
2023-08-08 13:31:48 +0530

    rest

---
+++ restproject/restapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    task_name = models.CharField(max_length=200)
+    task_desc = models.CharField(max_length=200)
+    date_created = models.DateTimeField(auto_now=True)
+    completed = models.BooleanField(default=False)
+# adding imagefield in API
+    image = models.ImageField(upload_to='images/',default="Image/None/Noimg.jpg")
